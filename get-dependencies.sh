8cf2da73b29f6500fe24f7ad4df57cab81d0ec03
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-14 00:23:40 -0400

    Create get-dependencies.sh

---
+++ get-dependencies.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+pacman -Syu --noconfirm \
+	base-devel       \
+	curl             \
+	git              \
+	lame             \
+	libxcb           \
+	libxcursor       \
+	libxi            \
+	libxkbcommon     \
+	libxkbcommon-x11 \
+	libxrandr        \
+	libxtst          \
+	musescore        \
+	pipewire-audio   \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	qt6ct            \
+	qt6-wayland      \
+	wget             \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-common --prefer-nano
+
+pacman -Q musescore | awk '{print $2; exit}' > ~/version
