ec926746f6578d306a148606da1ac0da3bc68de1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-14 04:19:57 +0000

    Add files via upload

---
+++ musescore-appimage.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+VERSION="$(cat ~/version)"
+
+export ADD_HOOKS="self-updater.bg.hook"
+export ICON=/usr/share/icons/hicolor/128x128/apps/mscore.png
+export DESKTOP=/usr/share/applications/org.musescore.MuseScore.desktop
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export DEPLOY_PIPEWIRE=1
+export OUTNAME=MuseScore-"$VERSION"-anylinux-"$ARCH".AppImage
+
+# Deploy dependencies
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/mscore
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
